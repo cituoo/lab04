2562bd576676e9dd85e0c36a75d6bf1ee317e626
YaroslavHome <cituoo@yandex.ru>
2017-10-01 19:09:55 +0300

    End lab02
    First send.

--- class_work.cpp
+++ class_work.cpp
@@ -2,91 +2,96 @@
 
 int main() {
 // initialization
-    std::pair<int, int> a;
-    std::cout << "Enter pair numbers X and Y:" << std::endl;
-    std::cin >> a.first >> a.second;
+    int x = 0;
+    int y = 0;
     int z = 0;
-    std::cout << "Enter Z:" << std::endl;
-    std::cin >> z;
+    std::cout << "Enter numbers X, Y and Z:" << std::endl;
+    std::cin >> x >> y >> z;
 
 // num1
-    if ((a.first % 2 != 0) && (a.second % 2 != 0))
+    std::cout << "1) Both numbers are odd:   ";
+    if ((x % 2 != 0) && (y % 2 != 0))
     {
-        std::cout << "1) True." << std::endl;
+        std::cout << "True." << std::endl;
     }
     else
     {
-        std::cout << "1) False." << std::endl;
+        std::cout << "False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // num2
-    if ((a.first < 20)&&(a.second > 20)||(a.first > 20)&&(a.second < 20))
+    std::cout << "2) Only one number is less than 20:   ";
+    if ((x < 20)&&(y > 20)||(x > 20)&&(y < 20))
     {
-        std::cout << "2) True." << std::endl;
+        std::cout << "True." << std::endl;
     } else
     {
-        std::cout << "2) False." << std::endl;
+        std::cout << "False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // num3
-    if ((a.first == 0)||(a.second == 0))
+    std::cout << "3) At least one of the numbers is 0:   ";
+    if ((x == 0)||(y == 0))
     {
-        std::cout << "3) True." << std::endl;
+        std::cout << "True." << std::endl;
     }
     else
     {
-        std::cout << "3) False." << std::endl;
+        std::cout << "False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // num4
-    if ((a.first < 0)&&(a.second < 0)&&(z < 0))
+    std::cout << "4) Each of the numbers is negative:   ";
+    if ((x < 0)&&(y < 0)&&(z < 0))
     {
-        std::cout << "4) True." << std::endl;
+        std::cout << "True." << std::endl;
     }
     else
     {
-        std::cout << "4) False." << std::endl;
+        std::cout << "False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // num5
-    if (((a.first % 5 == 0)&&(a.second % 5 != 0)&&(z % 5 != 0))||
-       ((a.first % 5 != 0)&&(a.second % 5 == 0)&&(z % 5 != 0))||
-       ((a.first % 5 == 0)&&(a.second % 5 == 0)&&(z % 5 != 0)))
+    std::cout << "5) Only one of the numbers is a multiple of five:   ";
+    if (((x % 5 == 0)&&(y % 5 != 0)&&(z % 5 != 0))||
+        ((x % 5 != 0)&&(y % 5 == 0)&&(z % 5 != 0))||
+        ((x % 5 == 0)&&(y % 5 == 0)&&(z % 5 != 0)))
     {
-        std::cout << "5) True." << std::endl;
+        std::cout << "True." << std::endl;
     }
     else
     {
-        std::cout << "5) False" << std::endl;
+        std::cout << "False" << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // num6
-    if ((a.first > 100)||(a.second > 100)||(z > 100))
+    std::cout << "6) at least one of the numbers is greater than 100:   ";
+    if ((x > 100)||(y > 100)||(z > 100))
     {
-        std::cout << "6) True." << std::endl;
+        std::cout << "True." << std::endl;
     }
     else
     {
-        std::cout << "6) False" << std::endl;
+        std::cout << "False" << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // chess initialization:
-    std::pair<int, int> firstChessField;
-    std::cout << "Enter boat position: " << std::endl;
-    std::cin >> firstChessField.first >> firstChessField.second;
-    std::pair<int, int> secondChessField;
-    std::cout << "Enter horse position: " << std::endl;
-    std::cin >> secondChessField.first >> secondChessField.second;
+    std::pair<int, int> attackingChessField;
+    std::cout << "Enter first figure position: " << std::endl;
+    std::cin >> attackingChessField.first >> attackingChessField.second;
+    std::pair<int, int> pownedChessField;
+    std::cout << "Enter second figure position: " << std::endl;
+    std::cin >> pownedChessField.first >> pownedChessField.second;
 
 // chess 1:
-    if ((firstChessField.first == secondChessField.first)||
-        (firstChessField.second == secondChessField.second))
+    if ((attackingChessField.first == pownedChessField.first)||
+        (attackingChessField.second == pownedChessField.second))
     {
         std::cout << "chess1) True." << std::endl;
     }
@@ -94,11 +99,11 @@ int main() {
     {
         std::cout << "chess1) False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // chess 2
-    if (abs(firstChessField.first - firstChessField.second) ==
-        abs(secondChessField.first - secondChessField.second))
+    if (abs(attackingChessField.first - attackingChessField.second) ==
+        abs(pownedChessField.first - pownedChessField.second))
     {
         std::cout << "chess2) True." << std::endl;
     }
@@ -106,11 +111,11 @@ int main() {
     {
         std::cout << "chess2) False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // chess 3
-    if (abs((firstChessField.first - secondChessField.first) == 1)||
-        (abs(firstChessField.second - secondChessField.second) == 1))
+    if (abs((attackingChessField.first - pownedChessField.first) == 1)||
+        (abs(attackingChessField.second - pownedChessField.second) == 1))
     {
         std::cout << "chess3) True." << std::endl;
     }
@@ -118,13 +123,13 @@ int main() {
     {
         std::cout << "chess3) False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
 
 // chess 4
-    if (((firstChessField.first == secondChessField.first)||
-        (firstChessField.second == secondChessField.second))||
-        (abs(firstChessField.first - firstChessField.second) ==
-         abs(secondChessField.first - secondChessField.second)))
+    if (((attackingChessField.first == pownedChessField.first)||
+         (attackingChessField.second == pownedChessField.second))||
+        (abs(attackingChessField.first - attackingChessField.second) ==
+         abs(pownedChessField.first - pownedChessField.second)))
     {
         std::cout << "chess4) True." << std::endl;
     }
@@ -132,6 +137,93 @@ int main() {
     {
         std::cout << "chess4) False." << std::endl;
     }
-    std::cout << "" <<std::endl;
+    std::cout << "" << std::endl;
+
+// chess 5.1
+    int pawnStep = 0;
+    if (attackingChessField.first == 2)
+    {
+        pawnStep = 2;
+    }
+    else
+    {
+        pawnStep = 1;
+    }
+    if ((attackingChessField.second == pownedChessField.second)&&
+        ((attackingChessField.first + pawnStep == pownedChessField.first)||
+         (attackingChessField.first + 1 == pownedChessField.first)))
+    {
+        std::cout << "chess5.1) True." << std::endl;
+    }
+    else
+    {
+        std::cout << "chess5.1) False." << std::endl;
+    }
+    std::cout << "" << std::endl;
+// chess 5.2
+    if (((attackingChessField.first + 1 == pownedChessField.first)&&
+         (attackingChessField.second + 1 == pownedChessField.second))||
+        (attackingChessField.first - 1 == pownedChessField.first)&&
+        (attackingChessField.second + 1 == pownedChessField.second))
+    {
+        std::cout << "chess5.2) True." << std::endl;
+    }
+    else
+    {
+        std::cout << "chess5.2) False." << std::endl;
+    }
+    std::cout << "" << std::endl;
+
+// задачи на цикл:
+// 1
+    for (int i = 1; i < 10; ++i)
+    {
+        std::cout << i << " x 7 = " << i*7 << std::endl;
+    }
+    std::cout << "" << std::endl;
+
+// 2
+    int composition8x15 = 1;
+    for (int i = 8; i <= 15; ++i)
+    {
+        composition8x15 *= i;
+    }
+    std::cout << "The composition btw 8x9x10x..x15 = " << composition8x15 << std::endl;
+    std::cout << "" << std::endl;
+
+// 3
+    int a = 0;
+    std::cout << "Enter start number: ";
+    std::cin >> a;
+    int composition21xa = 1;
+    for (int i = a; i <= 20; ++i)
+    {
+        composition21xa *= i;
+    }
+    std::cout << "The composition btw " << a << "x...x20 = " << composition21xa << std::endl;
+    std::cout << "" << std::endl;
+
+// 4
+    int b = 0;
+    std::cout << "Enter end number: ";
+    std::cin >> b;
+    int composition1xb = 1;
+    for (int i = 1; i <= b; ++i)
+    {
+        composition1xb *= i;
+    }
+    std::cout << "The composition btw " << "1x...x" << b << " = "<< composition1xb << std::endl;
+    std::cout << "" << std::endl;
+
+// 5
+    std::cout << "Enter start and end numbers: ";
+    std::cin >> a >> b;
+    int compositionaxb = 1;
+    for (int i = a; i <= b; i++)
+    {
+        compositionaxb *= i;
+    }
+    std::cout << "The composition btw " << a << "x...x" << b << " = "<< compositionaxb << std::endl;
+    std::cout << "" << std::endl;
     return 0;
 }
